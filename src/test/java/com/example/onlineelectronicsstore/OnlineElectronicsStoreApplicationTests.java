a256073f58c1c1901648d5f4cb1d52a7d2d61525
ShuMAhd <shuman@zykaika.ru>
2024-02-20 13:51:41 +0800

    first commit

---
+++ src/test/java/com/example/onlineelectronicsstore/OnlineElectronicsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineelectronicsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineElectronicsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
